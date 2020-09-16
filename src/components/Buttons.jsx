3da74f56b08cf4c21abb92a0f42cd00590074f84
The B <be.timane@gmail.com>
2020-09-16 16:20:21 +0200

    added react calculator

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Buttons.css'
+
+export default props => {
+    let classes = 'button '
+    classes += `${props.operation ? 'operation' : ''} ${props.double ? 'double' : ''} ${props.triple ? 'triple' : ''}`
+    return <button
+        onClick={e=>props.click && props.click(props.label)}  
+        className={classes}>
+        {props.label}
+    </button>
+}
